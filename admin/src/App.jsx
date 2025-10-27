bfd0e7c49c1b35799ea136583a1ee4a834b25da7
Prince Kumar <147418512+prince62058@users.noreply.github.com>
2025-10-28 03:15:26 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -11,7 +11,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const Url = 'http://localhost:4000';
+  const Url = 'https://foodweb-backend-nopk.onrender.com';
 
   return (
     <div>
