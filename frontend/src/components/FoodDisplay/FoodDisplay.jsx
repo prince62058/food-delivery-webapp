7926938545ba835ae0e3f65e66a6b6516e3ad6e8
Prince Kumar <147418512+prince62058@users.noreply.github.com>
2025-10-29 01:29:17 +0530

    Update FoodDisplay.jsx

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -5,19 +5,25 @@ import FoodItem from '../FoodItem/FoodItem'
 
 
 const FoodDisplay = ({category}) => {
-   const {food_list} = useContext(StoreContext);
+   const {food_list,isLoadingFood} = useContext(StoreContext);
 
   return (
     <div className='food-display' id='food-display'>
        <h2>Top dishes near you</h2>
-       <div className="food-display-list">
-        {food_list.map((item,index)=>{
-          if (category === "All" || category.toLowerCase() === item.category.toLowerCase()) {
-            return <FoodItem key={index} id={item._id} name={item.name} price={item.price} description={item.description} image={item.image}/>
-          }
-        
-        })}
-       </div>
+       {isLoadingFood ? (
+         <div className="loading-spinner">
+           <div className="spinner"></div>
+           <p>Loading...</p>
+         </div>
+       ) : (
+         <div className="food-display-list">
+           {food_list.map((item,index)=>{
+             if (category === "All" || category.toLowerCase() === item.category.toLowerCase()) {
+               return <FoodItem key={index} id={item._id} name={item.name} price={item.price} description={item.description} image={item.image}/>
+             }
+           })}
+         </div>
+       )}
     </div>
   )
 }
